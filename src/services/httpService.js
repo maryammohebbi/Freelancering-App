bb83e5052d5cbd0dec701a98f4a7b89ffaee7e3e
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-01-15 00:25:12 +0330

    handling 401 error, create new access token based on refresh token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,32 @@
 import axios from "axios"
 
 const BASE__URL = "http://localhost:5000/api"
-
 const app = axios.create({
     baseURL: BASE__URL,
     withCredentials: true
 })
+
+app.interceptors.request.use(
+    (res)=> res,
+    (err)=> Promise.reject(err)
+)
+
+app.interceptors.response.use(
+    (res)=> res,
+    async (err)=> {
+        const originalConfig = err.config 
+        if(err.response.status === 401 && !originalConfig._retry){
+            originalConfig._retry = true
+            try {
+                const data = await axios.get("/user/refresh-token", {withCredentials: true})
+                if(data) return app(originalConfig)
+            } catch (error) {
+                return Promise.reject(error)
+            }
+        }
+        return Promise.reject(err)
+    }
+)
 
 const http = {
     get: app.get,
