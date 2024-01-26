4e995a5793db6a331aae2ca31e08cbac536cf5c9
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-01-26 23:24:19 +0330

    delete project from owner dashboard added

--- src/features/project/ProjectRow.jsx
+++ src/features/project/ProjectRow.jsx
@@ -6,9 +6,12 @@ import { toPersianNumbersWithComma } from '../../utils/toPersianNumber'
 import { CiEdit, CiTrash } from "react-icons/ci"
 import Modal from '../../ui/Modal'
 import ConfirmDelete from '../../ui/ConfirmDelete'
+import useRemoveProject from './useRemoveProject'
 
 function ProjectRow({index, project}) {
     const [isDeleteOpen, setIsDeleteOpen] = useState(false)
+    const [isEditOpen, setIsEditOpen] = useState(false)
+    const {removeProject, isDeleting} = useRemoveProject()
 
   return (
     <Table.Row>
@@ -30,9 +33,16 @@ function ProjectRow({index, project}) {
         </td>
         <td>
             <div className="flex items-center gap-x-4">
-                <button>
-                    <CiEdit className='size-6 text-primary-900'/>
-                </button>
+
+                <>
+                    <button onClick={()=> setIsEditOpen(true)}>
+                        <CiEdit className='size-6 text-primary-900'/>
+                    </button>
+                    <Modal title={`ویرایش ${project.title}`} onClose={()=>setIsEditOpen(false)} open={isEditOpen}>
+
+                    </Modal>
+                </>
+
                 <>
                     <button onClick={()=> setIsDeleteOpen(true)}>
                         <CiTrash className='size-6 text-error'/>
@@ -41,10 +51,14 @@ function ProjectRow({index, project}) {
                         <ConfirmDelete 
                             resourceName={project.title} 
                             onClose={()=> setIsDeleteOpen(false)}
+                            onConfirm={()=> removeProject(project._id, {
+                                onSuccess: ()=> setIsDeleteOpen(false)
+                            })}
                             disabled={false}
                         />
                     </Modal>
                 </>
+
             </div>
         </td>
         <td>-</td>
