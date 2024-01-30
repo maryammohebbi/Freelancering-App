7057ec7084421d671d13c2aab5ed89e2d079e40c
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-01-30 21:54:48 +0330

    useLocalStorageState added and got used in darkmode context

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,9 +1,13 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import useLocalStorageState from "../hooks/useLocalStoragestate";
 
 const DarkModeContext = createContext()
 
 export function DarkModeProvider({children}){
-    const [isDarkMode, setIsDarkMode] = useState(false)
+    const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+        "isDarkMode",
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    )
 
     const toggleDarkMode = ()=> setIsDarkMode((prev) => !prev)
 
