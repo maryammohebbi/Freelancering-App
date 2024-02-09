0e6d7b41f9200ecbb65ce05d571fe47cda08651c
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-02-09 17:59:47 +0330

    404 page styles chaneged

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,24 +8,26 @@ function NotFound() {
     const navigate = useNavigate()
     const moveBack = useMoveBack()
   return (
-    <div className='flex justify-center pt-10'>
-        <div className='max-w-sm sm:max-w-lg lg:max-w-2xl flex flex-col items-center gap-y-8'>
-            <TbError404 className='text-secondary-800 w-40 h-40 md:w-96 md:h-96'/>
-            <p className='font-bold text-5xl'>اوپس! 😬</p>
-            <p className='font-bold text-3xl'>اینجا خبری نیست ...</p>
-            <div className='w-full flex justify-between'>
-                <button
-                    onClick={moveBack} 
-                    className='flex gap-x-2 text-lg items-center text-blue-900'>
-                    <HiArrowRight/>
-                    <span>برگشت</span>
-                </button>
-                <button 
-                    onClick={e=> navigate("/")}
-                    className='flex gap-x-2 text-lg items-center text-blue-900'>
-                    <span>خانه</span>
-                    <HiHome/>
-                </button>
+    <div className='bg-secondary-0 h-screen'>
+        <div className='flex justify-center pt-10 '>
+            <div className='max-w-sm sm:max-w-lg lg:max-w-2xl flex flex-col items-center gap-y-8'>
+                <TbError404 className='text-secondary-800 w-40 h-40 md:w-96 md:h-96'/>
+                <p className='font-bold text-5xl text-secondary-900'>اوپس! 😬</p>
+                <p className='font-bold text-3xl text-secondary-900'>اینجا خبری نیست ...</p>
+                <div className='w-full flex justify-between'>
+                    <button
+                        onClick={moveBack} 
+                        className='flex gap-x-2 text-lg items-center text-primary-900'>
+                        <HiArrowRight/>
+                        <span>برگشت</span>
+                    </button>
+                    <button 
+                        onClick={e=> navigate("/")}
+                        className='flex gap-x-2 text-lg items-center text-primary-900'>
+                        <span>خانه</span>
+                        <HiHome/>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
