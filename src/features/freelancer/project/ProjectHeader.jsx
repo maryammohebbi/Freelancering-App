dd2edc3e4e12d86e3871885569819f0e8172c5d3
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-02-16 20:29:42 +0330

    filter components, categories and sort added

--- src/features/freelancer/project/ProjectHeader.jsx
+++ src/features/freelancer/project/ProjectHeader.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import FilterDropDown from '../../../ui/FilterDropDown'
+import useCategories from '../../../hooks/useCategories'
 
 function ProjectHeader() {
+  const {transformedCategories} = useCategories()
+  const sortOptions = [
+    {
+      value: "latest",
+      label: "مرتب سازی (جدیدترین)"
+    },
+    {
+      value: "earliest",
+      label: "مرتب سازی (قدیمی ترین)"
+    },
+  ]
   return (
     <div className='flex items-center justify-between text-secondary-700 mb-8'>
         <h1 className="text-lg font-bold">لیست پروژه ها</h1>
-        <div>فیلتر ها</div>
+        <div className='flex gap-x-8 items-center'>
+          <FilterDropDown
+            options={sortOptions}
+            filterField="sort"
+          />
+
+          <FilterDropDown
+            options={[
+              {
+                value: "ALL",
+                label: "دسته بندی (همه)"
+              },
+              ...transformedCategories
+            ]}
+            filterField="category"
+          />
+        </div>
     </div>
   )
 }
---
+++ src/ui/Select.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Select({value, onChange, options}) {
+  return (
+    <select value={value} onChange={onChange} className='textField__input py-2 text-xs bg-secondary-0'>
+        {options.map((item)=> (<option key={item.value} value={item.value}>{item.label}</option>))}
+    </select>
+  )
+}
+
+export default Select
