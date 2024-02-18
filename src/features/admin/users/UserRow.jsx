221f68f583cc1e6c64593bd9f7e8529f73a4be36
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-02-18 22:40:27 +0330

    user status cell style added

--- src/features/admin/users/UserRow.jsx
+++ src/features/admin/users/UserRow.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import Table from '../../../ui/Table'
 
+const statusStyle = [
+    {
+        label: "رد شده",
+        className: "badge--danger"
+    },
+    {
+        label: " در انتظار تایید",
+        className: "badge--secondary"
+    },
+    {
+        label: "تایید شده",
+        className: "badge--success"
+    }
+]
+
+
 function UserRow({user, index}) {
+    const {name, email, phoneNumber, role, status} = user
   return (
     <Table.Row>
         <td>{index + 1}</td>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.phoneNumber}</td>
-        <td>{user.role}</td>
-        <td>{user.status}</td>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td>{phoneNumber}</td>
+        <td>{role}</td>
+        <td>
+            {
+                <span className={`badge ${statusStyle[status].className}`}>
+                    {statusStyle[status].label}
+                </span>
+            }
+        </td>
         <td>++</td>
     </Table.Row>
   )
