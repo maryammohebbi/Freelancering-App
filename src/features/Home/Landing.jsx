dc81d4b3f185f7b2692e576ec2e0c467d90b5c4d
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-04-18 01:06:19 +0330

    minor changes in styles

--- src/features/Home/Landing.jsx
+++ src/features/Home/Landing.jsx
@@ -46,6 +46,10 @@ const projectsList = [
     id: 11,
     title: "طراحی کارت ویزیت"
   },
+  {
+    id: 12,
+    title: "..."
+  },
 
 ]
 
@@ -58,20 +62,35 @@ function Landing() {
           className='w-[20rem] h-[13rem] sm:w-[30rem] sm:h-[15rem] md:w-[40rem] md:h-[20rem] 
           lg:w-[50rem] lg:h-[30rem] absolute top-[20%] bg-slate-400/30 rounded-2xl p-5' 
         >
-            <h1 className='text-slate-100 font-bold text-center sm:text-lg md:text-xl lg:text-2xl'>بهترین فریلنسرها را استخدام کنید.</h1>
-            <h3 className='text-slate-300 font-bold sm:text-sm md:text-lg lg:text-xl mb-2 lg:mb-5'>تنوع در پروژه ها:</h3>
-            <ul className='text-xs text-neutral-950 font-bold sm:text-sm md:text-xl lg:text-2xl flex flex-wrap gap-2'>
+            <h1 
+              className='text-slate-100 font-bold text-center 
+              sm:text-lg md:text-xl lg:text-2xl'>
+                بهترین فریلنسرها را استخدام کنید.
+            </h1>
+            <h3 
+              className='text-green-600 font-bold shadow-2xl
+              sm:text-sm md:text-lg lg:text-xl mb-2 lg:mb-5'>
+                تنوع در پروژه ها:
+            </h3>
+
+            <ul className='text-xs text-neutral-950 font-bold sm:text-sm md:text-xl lg:text-2xl flex flex-wrap gap-2 justify-center'>
               {
                 projectsList.map(project => 
                   <li
                     key={project.id} 
-                    className='border border-slate-50 p-1 lg:p-4 rounded-xl shadow-2xl hover:bg-green-950/45 hover:text-white transition-all duration-500'>
+                    className='border border-slate-50 p-1 lg:p-4 rounded-xl shadow-2xl 
+                      hover:bg-green-950/45 hover:text-white transition-all duration-500'
+                    >
                       {project.title}
                   </li>
                 )
               }
             </ul>
-            <h4 className='hidden md:block text-white font-bold text-xl text-center mt-4 lg:text-2xl lg:mt-8'>🌟همین الان پروژه ی خود را سفارش دهید.</h4>
+            <h4 
+              className='hidden md:block text-white font-bold text-xl shadow-2xl
+              text-center mt-4 lg:text-2xl lg:mt-8'>
+                🌟همین الان پروژه ی خود را سفارش دهید.
+            </h4>
       </div>
     </div>
   )
